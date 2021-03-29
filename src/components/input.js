a9473b728fe94f67a4474e10953c89c671d62516
Anatoliy Surmach <asurmach@rbc.ru>
2021-03-30 02:29:42 +0300

    done

--- src/components/input.js
+++ src/components/input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Input = ({ onChange, value }) => {
-    return <input onChange={ onChange } value={ value } />;
+const Input = ({ onChange, value, placeholder }) => {
+    return <input onChange={ onChange } value={ value } placeholder={ placeholder } />;
 }
 
 export default Input;
